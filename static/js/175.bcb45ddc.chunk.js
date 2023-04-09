64f901f6dc87e6c677b9c8eb80548d868d685c31
Insaf <vaironfop@Mail.ru>
2023-04-09 17:34:18 +0300

    Updates

---
+++ static/js/175.bcb45ddc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcutaway=self.webpackChunkcutaway||[]).push([[175],{2175:function(e,t,s){s.r(t);s(2791);var h=s(3869),u=s(184);t.default=function(e){var t=e.theme;return(0,u.jsxs)("div",{children:[(0,u.jsx)(h.h4,{theme:t}),(0,u.jsx)(h.p2,{theme:t}),(0,u.jsx)(h._Y,{theme:t}),(0,u.jsx)(h.CL,{theme:t}),(0,u.jsx)(h.IK,{theme:t})]})}}}]);
+//# sourceMappingURL=175.bcb45ddc.chunk.js.map
